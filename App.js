3a416353898407914786f5c53014f7bee84b87be
VajihuddinAhmed <vajihahmed@gmail.com>
2021-05-17 23:35:58 +0200

    timer-focus project initiated

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Platform } from 'react-native';
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
